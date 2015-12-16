9a9d77d1b286b37c1fd774d4b96767dda9a78b93
Stefano Chiesa <base278@gmail.com>
2015-12-16 14:33:40 +0800

    Add login api without session

---
+++ api.js
@@ -0,0 +1,39 @@
+var bodyparser = require('body-parser');
+var express = require('express');
+var status = require('http-status');
+var crypto = require('crypto');
+
+module.exports = function(wagner) {
+  var api = express.Router();
+
+  api.use(bodyparser.json());
+  api.use(bodyparser.urlencoded({
+    extended: true
+  }));
+
+  api.post('/login', wagner.invoke(function(User) {
+    return function(req, res) {
+      var user = req.body.user;
+      var password = req.body.password;
+
+      User.findOne({ where: {name: req.body.user }}).then(
+        function(user) {
+          var shasum = crypto.createHash('sha1');
+          shasum.update(password);
+          var digest = shasum.digest('hex');
+
+          if (digest === user.password) {
+            return res.json({ status: 'OK' });
+          } else {
+            return res.json({ status: 'FAILED' });
+          }
+        },
+        function(err) {
+          return res.json({ status: 'FAILED' });
+        }
+      )
+    };
+  }));
+
+  return api;
+}
