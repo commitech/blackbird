2392a2f572865d26ec6fea6951d146c00c477a51
Stefano Chiesa <base278@gmail.com>
2015-12-19 10:35:12 +0800

    Use dependancy injection for Duty model

--- api/duty.js
+++ api/duty.js
@@ -1,28 +1,21 @@
-var bodyparser = require('body-parser');
 var express = require('express');
-
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
 var Const = require('../const');
 
 module.exports = function(wagner) {
-  var Duty = require('../objects/duty')(wagner);
   var api = express.Router();
 
-  api.use(bodyparser.json());
-  api.use(bodyparser.urlencoded({
-    extended: true
-  }));
+  var Duty = wagner.invoke(function(Duty) {
+    return Duty;
+  });
 
   api.get('/get_duty', function(req, res) {
     if (!req.query.id) {
-      return res.json({ status: Const.FAILED_STATUS_MESSAGE, 
+      return res.json({ status: Const.STATUS.FAILED, 
                         comment: 'ID is not specified' });
     }
     Duty.getDuty(req.query.id, function(duty) {
-      res.json({status: Const.STATUS.OK,
-                  result: duty});
+      return res.json({ status: Const.STATUS.OK,
+                        result: duty });
     });
   });
 
