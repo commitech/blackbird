f5e51b517214cb731683243373eb56837c10dc41
Stefano Chiesa <base278@gmail.com>
2015-12-19 21:36:44 +0800

    Refactor duty test suite

--- tests/user.js
+++ tests/user.js
@@ -4,7 +4,7 @@ var superagent = require('superagent');
 var passportStub = require('passport-stub');
 var Const = require('../const');
 
-var URL_ROOT = 'http://localhost:3000/api/v1';
+var URL_ROOT = 'http://localhost:3000/api/v1/user';
 
 describe("User API Tests", function() {
   var User;
@@ -28,7 +28,7 @@ describe("User API Tests", function() {
   });
 
   it('cannot login with invalid credentials', function(done) {
-    var url = URL_ROOT + '/user/login';
+    var url = URL_ROOT + '/login';
     agent.post(url).send({
       username: 'admin',
       password: 'password2'
@@ -45,7 +45,7 @@ describe("User API Tests", function() {
   });
 
   it('can login with valid credentials', function(done) {
-    var url = URL_ROOT + '/user/login';
+    var url = URL_ROOT + '/login';
     agent.post(url).send({
       username: 'admin',
       password: 'password'
@@ -65,7 +65,7 @@ describe("User API Tests", function() {
   it('connection persist after login', function(done) {
     passportStub.login({ name: 'admin' });
 
-    agent.get(URL_ROOT + '/user/me').end(function(err, res) {
+    agent.get(URL_ROOT + '/me').end(function(err, res) {
       assert.equal(res.status, 200);
       var json;
       assert.doesNotThrow(function() {
@@ -82,7 +82,7 @@ describe("User API Tests", function() {
   it('can logout after login', function(done) {
     passportStub.login({ name: 'admin' });
 
-    var logoutUrl = URL_ROOT + '/user/logout';
+    var logoutUrl = URL_ROOT + '/logout';
     agent.get(logoutUrl).
     end(function(err, res) {
       assert.equal(res.user, null);
